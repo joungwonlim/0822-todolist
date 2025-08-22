d8a2a341c3238644f97309477cbcd87397290b51
joungwonlim <joungwonlim@gmail.com>
2025-08-22 20:38:59 +0900

    feat: Re-apply design principles and refactor To-Do List (clean setup)

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
+        {children}
       </body>
     </html>
   );
